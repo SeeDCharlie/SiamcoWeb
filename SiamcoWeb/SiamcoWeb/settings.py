6ca2d8a52246826aebbd178fb8419cf35b89981f
seed <seed@seedpc>
2020-07-23 08:02:51 -0500

    23/jul/2020_0

--- SiamcoWeb/SiamcoWeb/settings.py
+++ SiamcoWeb/SiamcoWeb/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'generateCot.apps.GeneratecotConfig',
-
 ]
 
 MIDDLEWARE = [
--- SiamcoWeb/generateCot/views.py
+++ SiamcoWeb/generateCot/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from SiamcoWeb import settings
+import urllib
+import requests
+import json
 
 # Create your views here.
 
 def homeLoggin(request):
-    return render(request, 'generateCot/homeLoggin.html') 
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+
+        captchaKey = request.POST.get('g-recaptcha-response')
+        capt_url = "https://google.com/recaptcha/api/siteverify"
+        cap_secretKey = "6Lev1bQZAAAAAMDLb7YJtfLfZIIVJjtdPtl3h4Wj"
+        cap_data = {'secret': cap_secretKey, 'response': captchaKey}
+        cap_server_response = requests.post(url = capt_url, data = cap_data)
+        capJson = json.loads(cap_server_response.text)
+        print("data json : ", capJson)
+        print("resultado : " , capJson['success'])
+
+        return render(request, 'generateCot/homeLoggin.html')
+    else:
+        return render(request, 'generateCot/homeLoggin.html') 
