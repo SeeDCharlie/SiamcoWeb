2a65a148a8ca6229426e7e7c43d3a7d6c24a4051
seed <seed@seedpc>
2020-08-11 14:42:00 -0500

    11/ago/2020_0

--- SiamcoWeb/generateCot/static/generateCot/general/generatorCot/datatables/controlTables.js
+++ SiamcoWeb/generateCot/static/generateCot/general/generatorCot/datatables/controlTables.js
@@ -1,43 +1,56 @@
-function builRow(dats, id){
+
+$(document).ready(function () {
+    $('#editor').wysiwyg({
+
+    });
+
+});
+
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
+function builRow(dats, id) {
     var row = '<tr id ="rcot"+id>';
 
-    $.each(dats, function(idx,dat){
+    $.each(dats, function (idx, dat) {
         row += '<td>' + dat + '</td>';
     });
-    row += '<td>' + parseInt(dats[2],10)*parseInt(dats[3],10) + '</td>';
-    row += '<td> <button type="button" class="btn"  value="'+id+'"><i class="fa fa-times-circle" style="color: red;" aria-hidden="true" i> </button> </td>';
+    row += '<td>' + parseInt(dats[2], 10) * parseInt(dats[3], 10) + '</td>';
+    row += '<td> <button type="button" class="btn"  value="' + id + '"><i class="fa fa-times-circle" style="color: red;" aria-hidden="true" i> </button> </td>';
 
     row += "<tr>";
     return row;
 }
 
-function appendRow(row){
+function appendRow(row) {
     var table = $("#tabledos tbody");
     table.append(row);
 }
 
 $('button').click(function (e) {
 
-    var buttonVal = $(this).val() ;
-    var parent = $(this).parent().get(0).tagName  ;
+    var buttonVal = $(this).val();
+    var parent = $(this).parent().get(0).tagName;
     ///alert("valor del boton : " + buttonVal + " parent name : " );
 
-    if (parent == "TD"){
-        
+    if (parent == "TD") {
+
 
         dats = [
-            $("#d"+buttonVal).html(),
-            $("#u"+buttonVal).html(),
-            $("#c"+buttonVal).val(),
-            $("#v"+buttonVal).val()
+            $("#d" + buttonVal).html(),
+            $("#u" + buttonVal).html(),
+            $("#c" + buttonVal).val(),
+            $("#v" + buttonVal).val()
         ];
 
-        if (!$.isNumeric(dats[2]) || !$.isNumeric(dats[3]) ){
+        if (!$.isNumeric(dats[2]) || !$.isNumeric(dats[3])) {
             alert("Cantidad o Valor incorrectos : Por favor digite numeros correctos");
         }
-        else{
+        else {
             appendRow(builRow(dats, buttonVal));
         }
-    }    
+    }
 
 });
