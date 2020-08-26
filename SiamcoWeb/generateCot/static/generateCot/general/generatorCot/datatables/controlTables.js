03bbd4625876e1ba12c48fa3b6965c460990d727
seed <seed@seedpc>
2020-08-26 10:16:34 -0500

    26/ago/2020_0

--- SiamcoWeb/generateCot/static/generateCot/general/generatorCot/datatables/controlTables.js
+++ SiamcoWeb/generateCot/static/generateCot/general/generatorCot/datatables/controlTables.js
@@ -1,4 +1,42 @@
+function builRow(dats) {
+    var row = '';
+    row += '<tr>';
+    $.each(dats, function (idx, dat) {
+        row += '<td id="dat' + idx + '">' + dat + '</td>';
+    });
+    row += '<td id="dat4">' + parseInt(dats[2], 10) * parseInt(dats[3], 10) + '</td>';
+    row = row + '<td> <button type="button" class="btn fa fa-times-circle" style="color: red;"  value=""> </button> </td>';
+    row += "<tr>";
+    return row;
+}
 
+function appendRow(row) {
+    var table = $("#tabledos tbody");
+    table.append(row);
+}
+
+function getSum() {
+    var sum = 0;
+    $('#tabledos tbody tr').each(function (idx, dat) {
+        var vt = $(this).find("#dat4").text();
+        if ($.isNumeric(vt)) {
+            sum += parseFloat(vt);
+        }
+    });
+    return sum;
+}
+
+function changeValueCot() {
+    var dat = getSum();
+
+    $('#tmom').html(":$" + dat);
+    $('#ta').html(":$" + (dat * 0.11));
+    $('#ti').html(":$" + (dat * 0.05));
+    $('#tu').html(":$" + (dat * 0.05));
+    $('#tiu').html(":$" + (dat * 0.19));
+    $('#tdi').html(":$" + (dat + (dat * 0.11) + (dat * 0.05) + (dat * 0.05) + (dat * 0.19)));
+
+}
 
 $(document).ready(function () {
 
@@ -6,85 +44,32 @@ $(document).ready(function () {
         fullPage: true,
     });
 
-    function builRow(dats) {
-        var row = '';
+});
 
-        row += '<tr>';
-        $.each(dats, function (idx, dat) {
+$(document).on('click', '#tableuno button', function (e) {
+    var buttonVal = $(this).val();
+    //alert("add buttom:" );
 
-            row += '<td id="dat' + idx + '">' + dat + '</td>';
-        });
-        row += '<td id="dat4">' + parseInt(dats[2], 10) * parseInt(dats[3], 10) + '</td>';
-        row += '<td> <button type="button" class="btn"  value=" "><i class="fa fa-times-circle" style="color: red;" aria-hidden="true" i> </button> </td>';
-
-        row += "<tr>";
-
-
-        return row;
+    dats = [
+        $("#d" + buttonVal).html(),
+        $("#u" + buttonVal).html(),
+        $("#c" + buttonVal).val(),
+        $("#v" + buttonVal).val()
+    ];
+    if (!$.isNumeric(dats[2]) || !$.isNumeric(dats[3])) {
+        alert("Cantidad o Valor incorrectos : Por favor digite numeros correctos");
+    }
+    else {
+        appendRow(builRow(dats));
+        changeValueCot();
     }
 
-    function appendRow(row) {
-        var table = $("#tabledos tbody");
-        table.append(row);
-    }
+});
 
-    function getSum() {
-        var sum = 0;
-        $('#tabledos tbody tr').each(function (idx, dat) {
-            var vt = $(this).find("#dat4").text();
-
-            if ($.isNumeric(vt)) {
-                sum += parseFloat(vt);
-            }
-
-        });
-        return sum;
-    }
-
-    function changeValueCot() {
-        var dat = getSum();
-
-        $('#tmom').html(":$"+(dat*1.0));
-        $('#ta').html(":$"+(dat*0.11))  ;
-        $('#ti').html(":$"+(dat*0.05))  ;
-        $('#tu').html(":$"+(dat*0.05))  ;
-        $('#tiu').html(":$"+(dat*0.19))  ;
-        $('#tdi').html(":$"+((dat + dat*0.05)+(dat*0.11)+(dat*0.05)+(dat*0.05)+(dat*0.19) ))  ;
-      
-
-    }
-
-    $('button').click(function (e) {
-
-        var buttonVal = $(this).val();
-        var parent = $(this).parent().get(0).tagName;
-        ///alert("valor del boton : " + buttonVal + " parent name : " );
-
-        if (parent == "TD") {
-
-
-            dats = [
-                $("#d" + buttonVal).html(),
-                $("#u" + buttonVal).html(),
-                $("#c" + buttonVal).val(),
-                $("#v" + buttonVal).val()
-            ];
-
-            if (!$.isNumeric(dats[2]) || !$.isNumeric(dats[3])) {
-                alert("Cantidad o Valor incorrectos : Por favor digite numeros correctos");
-            }
-            else {
-
-
-                appendRow(builRow(dats));
-                changeValueCot();
-
-            }
-        }
-
-    });
-
-
+$(document).on('click', '.tabledos button', function () {
+    //alert("se eliminara : " + $(this).parent().parent().text());
+    $(this).parent().parent().remove();
+    changeValueCot();
 });
 
 
--- SiamcoWeb/generateCot/static/generateCot/general/generatorCot/datatables/petAjax.js
+++ SiamcoWeb/generateCot/static/generateCot/general/generatorCot/datatables/petAjax.js
@@ -1,86 +1,85 @@
 //send data to server
-$(function () {
 
-    $(document).on('click', '#bGenerate', function (e) {
-        e.preventDefault(e);
-        var date = new Date();
-        date = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDay();
-        durationW = $("#dProject").val();
-        if (!$.isNumeric(durationW)) {
-            durationW = 0;
-        }
-        var al = getListActivities();
-        var totalCot = [
-            $('#tmom').text().substr(2),
-            $('#ta').text().substr(2),
-            $('#ti').text().substr(2),
-            $('#tu').text().substr(2),
-            $('#tiu').text().substr(2),
-            $('#tdi').text().substr(2)
-        ];
-        
-        $.ajax({
-            url: $("#bGenerate").attr('urlDestinity'),
-            data: {
+$(document).on('click', '#bGenerate', function (e) {
+    e.preventDefault(e);
+    var date = new Date();
+    date = date.getFullYear() + '-' + (date.getMonth() + 1 ) + '-' + date.getDate();
+    alert("la fecha es : " + date);
+    durationW = $("#dProject").val();
+    if (!$.isNumeric(durationW)) {
+        durationW = 0;
+    }
+    var al = getListActivities();
+    var totalCot = [
+        $('#tmom').text().substr(2),
+        $('#ta').text().substr(2),
+        $('#ti').text().substr(2),
+        $('#tu').text().substr(2),
+        $('#tiu').text().substr(2),
+        $('#tdi').text().substr(2)
+    ];
 
-                datsCot: JSON.stringify({
-                    customerName: $("#nClient").val(),
-                    customerEmail: $("#emClient").val(),
-                    placeName: $("#nPlace").val(),
-                    placeAddress: $("#dPlace").val(),
-                    projectName: $("#nProject").val(),
-                    proposalNumber: $("#nPropuesta").val(),
-                    durationWork: durationW,
-                    unitDuration: $("#dUnd").val(),
-                    autorName: $("#fName").text() + $("#lName").text(),
-                    username: $('#UsernameAux').text(),
-                    dateToday: date,
-                    actiList: al,
-                    totals : totalCot,
-                    //otherStyle: $('iframe').contents().find('head').find('style').html()
-                    textCot: getTextCot(),
-                    textNote: $('#textNote').val()
+    $.ajax({
+        url: $("#bGenerate").attr('urlDestinity'),
+        data: {
 
-                }),
-                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
-                action: 'post'
-            },
-            type: 'POST',
-            dataType: 'json',
-            success: function (data) {
-                if (data.isRender) {
+            datsCot: JSON.stringify({
+                customerName: $("#nClient").val(),
+                customerEmail: $("#emClient").val(),
+                placeName: $("#nPlace").val(),
+                placeAddress: $("#dPlace").val(),
+                projectName: $("#nProject").val(),
+                proposalNumber: $("#nPropuesta").val(),
+                durationWork: durationW,
+                unitDuration: $("#dUnd").val(),
+                autorName: $("#fName").text() + $("#lName").text(),
+                username: $('#UsernameAux').text(),
+                dateToday: date,
+                actiList: al,
+                totals: totalCot,
+                //otherStyle: $('iframe').contents().find('head').find('style').html()
+                textCot: getTextCot(),
+                textNote: $('#textNote').val()
 
-                    $('#f_generateCot').append('<input type="hidden" name="username" value="' + data.username + '" />');
-                    $('#f_generateCot').submit();
+            }),
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+            action: 'post'
+        },
+        type: 'POST',
+        dataType: 'json',
+        success: function (data) {
+            if (data.isRender) {
 
-                }
-            },
-            error: function () {
-                alert("no paso nada con el ajax!!");
+                $('#f_generateCot').append('<input type="hidden" name="username" value="' + data.username + '" />');
+                $('#f_generateCot').submit();
+
             }
-
-        });
+        },
+        error: function () {
+            alert("no paso nada con el ajax!!");
+        }
 
     });
 
-    function getListActivities() {
-        var dataStore = [];
-        $('#tabledos tbody tr').each(function (idx,dat) {
-            var td = [$(this).find("#dat0").text(),
-            $(this).find("#dat1").text(),
-            $(this).find("#dat2").text(),
-            $(this).find("#dat3").text(),
-            $(this).find("#dat4").text(),
-            ];
-            
-            dataStore.push(td);
-        });
-        return dataStore;
-
-    }
-    function getTextCot(){
-        $('iframe').contents().find('table').addClass("tableDos");
-        return $('iframe').contents().find('body').html();
-    }
-
 });
+
+function getListActivities() {
+    var dataStore = [];
+    $('#tabledos tbody tr').each(function (idx, dat) {
+        var td = [$(this).find("#dat0").text(),
+        $(this).find("#dat1").text(),
+        $(this).find("#dat2").text(),
+        $(this).find("#dat3").text(),
+        $(this).find("#dat4").text(),
+        ];
+
+        dataStore.push(td);
+    });
+    return dataStore;
+
+}
+function getTextCot() {
+    $('iframe').contents().find('table').addClass("tableDos");
+    return $('iframe').contents().find('body').html();
+}
+
